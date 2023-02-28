6ed24fd0c28d092b1ed37d7b19251b5c80ed6b0f
emmanuel-storm <innocent.lattier@esdes.ucly.fr>
2023-02-28 12:34:17 +0100

    Create login page + create generics components inputText and buttons

---
+++ frontend/src/components/Input/Button/PrimaryButton/PrimaryButton.styles.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled"
+import Button from '@mui/material/Button'
+
+export const ButtonComponent = styled(Button)`
+  width: 30rem;
+  background-color: #FDF4EB; // à rajouter dans le file theme.ts
+  color: #D99E63;
+  
+  &:hover {
+    background-color: #D99E63;
+    color: white;
+  }
+`
