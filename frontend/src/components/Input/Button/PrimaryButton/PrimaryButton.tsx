278a49e97043b8c72b92f9d4ad644c7ee37c27c4
Nathan.P <nathan.poncet@crea-inseec.com>
2023-02-28 15:29:04 +0100

    Create Dashbord / home page Fixes #3

--- frontend/src/components/Input/Button/PrimaryButton/PrimaryButton.tsx
+++ frontend/src/components/Input/Button/PrimaryButton/PrimaryButton.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Box from '@mui/material/Box';
 import * as Styled from './PrimaryButton.styles';
 
 export const PrimaryButton: React.FC<PrimaryButtonProps> = (props) => {
