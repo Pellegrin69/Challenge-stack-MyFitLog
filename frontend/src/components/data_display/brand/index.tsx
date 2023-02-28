8261f761d0f649ebd1c0b7c10419f392e2ce0dab
Nathan.P <nathan.poncet@crea-inseec.com>
2023-02-28 09:32:58 +0100

    Create Header and footer Fixes #6

--- frontend/src/components/data_display/brand/index.tsx
+++ frontend/src/components/data_display/brand/index.tsx
@@ -1,10 +1,13 @@
 import { styled } from '@mui/system';
+import { Link } from 'react-router-dom';
 import * as Styles from './index.styles';
 
 export const Brand = () => {
   return (
-    <Styles.Container variant="h5" variantMapping={{ h5: 'h1' }}>
-      <span>MyFit</span>Log
-    </Styles.Container>
+    <Link to={`/`}>
+      <Styles.Container variant="h5" variantMapping={{ h5: 'h1' }}>
+        <span>MyFit</span>Log
+      </Styles.Container>
+    </Link>
   );
 };
