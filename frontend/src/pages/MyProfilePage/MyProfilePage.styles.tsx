80fd24fc100cec9607d38f735203c878e1df88dd
emmanuel-storm <innocent.lattier@esdes.ucly.fr>
2023-03-01 16:31:37 +0100

    create myProfile page

--- frontend/src/pages/MyProfilePage/MyProfilePage.styles.tsx
+++ frontend/src/pages/MyProfilePage/MyProfilePage.styles.tsx
@@ -1,20 +1,20 @@
-import styled from "@emotion/styled"
+import { Card } from "@mui/material"
+import { styled } from "@mui/system"
 
-export const Container = styled.div`
-  height: 100vh;
+export const Container = styled("div")`
+  
+`
+
+export const LeftContainer = styled("div")`
+  padding: 6rem;
+`
+
+export const InformationCard = styled(Card)`
+  padding: 2rem;
   display: grid;
-  grid-template-columns: 10% 60% 30%;
-  background-color: #F5F4EE;
-`
-
-export const LeftContainer = styled.div`
-  background-color: white;
-`
-
-export const MiddleContainer = styled.div`
-  background-color: white;
-`
-
-export const RightContainer = styled.div`
-  background-color: gray;
+  grid-template-rows: 70% 30%;
+  height: 10rem;
+  border-radius: 1rem;
+  margin-bottom: 2rem;
+  width: 80%;
 `
